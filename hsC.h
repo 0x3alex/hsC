05735cf81fb69099d8145d856a0800ecf67de10b
0x3Alex <a-wrede@outlook.de>
2022-10-02 11:28:16 +0200

    some new functions

--- hsC.h
+++ hsC.h
@@ -6,17 +6,29 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+/*
+    * Struct for dropWhile and takeWhile to store length
+*/
+
+typedef struct {
+    void *res;
+    int len;
+} res;
 
 /*
     * No general typed functions for saftey
 */
-int     *mapIntToInt(int(*f)(int), int *list, int len);
-int     *takeInt(int amount, int *list, int len);
-int     *takeWhileInt(bool(*f)(int),int *list, int len);
-int     *dropWhileInt(bool(*f)(int),int *list, int len);
+int     *iiMap(int(*f)(int), int *list, int len);
+int     *iTake(int amount, int *list, int len);
+res     *iTakeWhile(bool(*f)(int),int *list, int len);
+res     *iDropWhile(bool(*f)(int),int *list, int len);
+int     *iGen(int start, int stop);
+int     *iGenFilter(int start, int stop, bool(*f)(int));
 
 
-char        *mapChar(char(*f)(char),char *list, int len);
+char        *ccMap(char(*f)(char),char *list, int len);
 char        *takeChar(int amount, char *list, int len);
 char        *takeWhileChar(bool(*f)(char),int *list, int len);
 char        *dropWhileChar(bool(*f)(char),int *list, int len);
@@ -30,8 +42,11 @@ char        *dropWhileChar(bool(*f)(char),int *list, int len);
 
 #ifdef hsC_C
 
-int *mapIntToInt(int(*f)(int), int* list, int len) {
+/* Int */
+
+int *iiMap(int(*f)(int), int* list, int len) {
     int *t = calloc(len,sizeof(int));
+    if(t == NULL) return NULL;
     for(int i = 0; i < len; i++) {
         t[i] = f(list[i]);
     }
@@ -39,22 +54,92 @@ int *mapIntToInt(int(*f)(int), int* list, int len) {
 
 }
 
-int *takeInt(int amount, int *list, int len) {
+int *iTake(int amount, int *list, int len) {
     if(amount > len) return NULL;
-    int p = calloc(amount,sizeof(int));
+    int *p = calloc(amount,sizeof(int));
+    if(p == NULL) return NULL;
     for(int i = 0; i < amount; i++) {
         p[i] = list[i];
     }
     return p;
 }
 
-char *mapChar(char(*f)(char),char* list, int len) {
-    int *t = calloc(len,sizeof(char));
+res *iTakeWhile(bool(*f)(int),int *list, int len) {
+    /*
+    *   Create new list with same size, since its not known how many elements will be in int
+    */  
+    int *tmp = calloc(len,sizeof(int));
+    if(tmp == NULL) return NULL;
+    int i = 0;
+
+    /*
+    *   Copy until false result is reached
+    */
+    for(; i <= len; i++) {
+        if(f(list[i]) == true) {
+            
+            tmp[i] = list[i];
+            continue;
+        }
+        break;  
+    }
+
+    /*
+    *   create new list with exact needed length
+    */
+    int *r = calloc(i,sizeof(int));
+
+    /*
+    *   Copy the results
+    */
+    memcpy(r,tmp,i*sizeof(int));
+    free(tmp);
+
+    /*
+    *   To provide the length return a struct, which contains length and result
+    */
+    res *re = calloc(1,sizeof(res));
+    re ->res = (void*) r;
+    re ->len = i;
+    return re;
+}
+
+int *iGen(int start, int stop) {
+    int *p = NULL;
+    int index = 0;
+    /*
+        *Gen descending list
+    */
+    if(stop < start) {
+        p = calloc((start-stop)+1,sizeof(int));
+        if (p == NULL) return NULL;
+        for( ; start >= stop ;) {
+            p[index++] = start--;
+        }
+    }
+
+    /*
+        *Gen ascending list
+    */
+    p = calloc((stop-start)+1,sizeof(int));
+    if (p == NULL) return NULL;
+    for( ; start <= stop ; ) {
+        p[index++] = start++;
+    }
+    return p;
+    
+}
+
+
+/* String */
+
+char *ccMap(char(*f)(char),char *list, int len) {
+    char *t = calloc(len,sizeof(char));
+    if(t == NULL) return NULL;
     for(int i = 0; i < len; i++) {
         t[i] = f(list[i]);
     }
     return t;
-
 }
 
 #endif
--- test.c
+++ test.c
@@ -8,24 +8,33 @@ int timeTwo(int a) {
     return a*2;
 }
 
-bool isSix(int a) {
-    return a == 6;
+bool isLessThen10(int a) {
+    return a < 10;
 }
 
 int main(void) {
 
-    int l = 3;
-    int a[l];
-    
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
+    int len = 10;
 
-    int *n = mapIntToInt(&timeTwo,a,l);
-
-    for(int i = 0; i < l; i++) {
+    puts("iiMap");
+    int *n = iiMap(&timeTwo,iGen(0,len),len);
+    for(int i = 0; i < len; i++) {
         printf("%d\n",n[i]);
     }
+    puts("iTake");
+    int *j = iTake(3,n,len);
+    for(int i = 0; i < 3; i++) {
+        printf("%d\n",j[i]);
+    }
+    puts("iTakeWhile");
+
+    res *k = iTakeWhile(&isLessThen10,n,len);
+    for(int i = 0; i < (k ->len); i++) {
+        printf("%d\n",((int*)(k->res))[i]);
+    }
+    free(k);
+    free(j);
+    free(n);
 
     return 0;
 }
