82c57991543a14a9e7dc7fd76de935b48dc123a9
0x3Alex <a-wrede@outlook.de>
2022-10-06 15:25:05 +0200

    isInfixOf and example

---
+++ examples/isInfixOf_example.c
@@ -0,0 +1,16 @@
+#define hsC_C
+
+#include "../hsC.h"
+#include <stdio.h>
+
+
+int main(void) {
+    
+    char *text = "hello world and hello mars!";
+    char *s = "and";
+    bool f = NULL;
+    isInfixOf(s,3,char*,text,27,f);
+    printf("Is %s infix of %s? %s",s,text,f ? "Yes" : "No");
+    return 0;
+}
+
